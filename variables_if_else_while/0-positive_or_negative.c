c4947015aa4890ca094f70322fd21c10ba1497b2
root <ashanti.chaplin@atlasschool.com>
2024-01-23 02:13:44 +0000

    variable 2

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h> 
+#include <stdio.h>
+/**
+ *  main - this is the main function
+ *  Return: always 0
+ */
+
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
