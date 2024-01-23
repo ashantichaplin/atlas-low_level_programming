7e3a38b7bf5508b67fee9f9664397c11ff83c640
root <ashanti.chaplin@atlasschool.com>
2024-01-23 04:21:02 +0000

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry function
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+
+}
