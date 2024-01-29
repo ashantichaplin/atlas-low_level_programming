33e9a016cd71bd1f770a0e11d73f38e37c48b338
ashantichaplin <ashanti.chaplin@atlasschool.com>
2024-01-29 23:09:43 +0000

    ..

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Confirm upper case
+ * @x: The number to review
+ * Return: 1 uppercase or 0 for anything else
+ * /
+
+int _isupper(int x)
+
+{
+
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
