ff8b159b3d24e426e77cf902328baa6348703db1
ashantichaplin <ashanti.chaplin@atlasschool.com>
2024-01-30 01:01:11 +0000

    numbers, from 0 to 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more number - function that prints 10 times the numbers 0 to 14
+ * Return: 10 times of the numbers since 0 up 14
+*/
+
+void more_number(void)
+
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+	for (y = 0; y <= 14; y++);
+	{
+	if (y > 9)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
+	}
+	_putchar('\n');
+	}
+
+}
