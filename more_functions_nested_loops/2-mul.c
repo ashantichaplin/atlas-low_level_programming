0eb14bb9cee03ef10924028b53a841ac87e9ac17
ashantichaplin <ashanti.chaplin@atlasschool.com>
2024-01-30 00:00:38 +0000

    updated *

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul -Multiplicatin 2 int
+ * @x: Multiply first
+ * @y: Multiply second with x
+ * Return: The product of multiplying x and y
+ */
+
+int mul(int x, int y)
+
+{
+	return (x * y);
+
+}
