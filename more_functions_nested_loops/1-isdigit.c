c401e939776d427984ebf202e0b3d1d4973d93da
ashantichaplin <ashanti.chaplin@atlasschool.com>
2024-01-30 00:23:54 +0000

    updated

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -4,16 +4,14 @@
  * _isdigit - Confirm character is a digit
  * @x: Number to confirm
  * Return: 1 for a character that is a digit 0 for all else
- */
+*/
 
 int _isdigit(int x)
 
 {
-	
 	if (x >= 48 && x <= 57)
 	{
 	return (1);
 	}
 	return (0);
-
 }	
